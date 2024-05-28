93022e620d5ce95550451cb351caf6a9547f11c1
backkom <backkom@kookmin.ac.kr>
2024-05-28 15:28:54 +0900

    1-03 controller 생성

---
+++ src/main/java/com/example/javarepublic/JavarepublicApplication.java
@@ -0,0 +1,13 @@
+package com.example.javarepublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarepublicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javarepublic/JavarepublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarepublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarepublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
