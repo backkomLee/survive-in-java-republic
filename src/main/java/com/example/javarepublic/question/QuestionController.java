ad72ec9e2ad48b2bc8c8d3b040028d4d290705e6
backkom <backkom@kookmin.ac.kr>
2024-05-29 14:28:41 +0900

    2-16 질문 등록 기능 추가

--- src/main/java/com/example/javarepublic/question/QuestionController.java
+++ src/main/java/com/example/javarepublic/question/QuestionController.java
@@ -7,8 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import com.example.javarepublic.answer.AnswerForm;
+
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -16,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 public class QuestionController {
 
     private final QuestionService questionService;
-
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -26,9 +31,23 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
